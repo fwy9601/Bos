ed9d8a64beb0f3488a6935de2e4c53b29af902cb
dusto <fwy9601@126.com>
2018-12-27 22:27:53 +0800

    提交初始的BaseAction

---
+++ bos-web/src/main/java/com/dusto/bos/web/action/base/BaseAction.java
@@ -0,0 +1,31 @@
+package com.dusto.bos.web.action.base;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+public class BaseAction<T> extends ActionSupport implements ModelDriven<T> {
+
+    //模型对象
+    private T model;
+    public T getModel() {
+        return model;
+    }
+
+    //在构造方法中动态获取实体类型，通过反射创建model对象
+    public BaseAction(){
+        ParameterizedType superclass = (ParameterizedType)this.getClass().getGenericSuperclass();
+        Type[] actualTypeArguments = superclass.getActualTypeArguments();
+        Class<T> entityClass = (Class<T>)actualTypeArguments[0];
+        try {
+            model = entityClass.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        
+    }
+}
