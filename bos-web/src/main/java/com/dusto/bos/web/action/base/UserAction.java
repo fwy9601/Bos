d8c51eb501e21b77b4a02b4433832ef99633b7ef
fanwangyong <1330219191@qq.com>
2018-12-28 12:59:45 +0800

    添加UserAction

---
+++ bos-web/src/main/java/com/dusto/bos/web/action/base/UserAction.java
@@ -0,0 +1,10 @@
+package com.dusto.bos.web.action.base;
+
+import org.springframework.stereotype.Controller;
+
+import com.dusto.bos.domain.User;
+
+@Controller
+public class UserAction extends BaseAction<User>{
+    
+}
