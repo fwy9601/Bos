046e384f424e1484b567bd84a3102ecea95896f2
fanwangyong <1330219191@qq.com>
2019-01-07 16:52:59 +0800

    为完成的分页条件查询

--- bos-web/src/main/java/com/dusto/bos/web/action/SubareaAction.java
+++ bos-web/src/main/java/com/dusto/bos/web/action/SubareaAction.java
@@ -1,9 +1,13 @@
 package com.dusto.bos.web.action;
 
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
+import com.dusto.bos.domain.Region;
 import com.dusto.bos.domain.Subarea;
 import com.dusto.bos.service.ISubareaService;
 import com.dusto.bos.web.action.base.BaseAction;
@@ -31,6 +35,29 @@ public class SubareaAction extends BaseAction<Subarea> {
      * @return
      */
     public String pageQuery() {
+        DetachedCriteria db = pageBean.getDetachedCriteria();
+        //动态添加过滤条件
+        String addresskey = model.getAddresskey();
+        if(StringUtils.isNoneBlank(addresskey)){
+            //添加过滤条件，根据地址关键字模糊查询
+            db.add(Restrictions.like("addresskey", "%"+addresskey+"%"));
+        }
+        Region region = model.getRegion();
+        if(region!=null){
+            String province = region.getProvince();
+            String city = region.getCity();
+            String district = region.getDistrict();
+            if(StringUtils.isNoneBlank(province)){
+                //添加过滤条件，根据省份关键字模糊查询
+                db.add(Restrictions.like("addresskey", "%"+province+"%"));
+            }
+            if(StringUtils.isNoneBlank(city)){
+                  //添加过滤条件，根据地址关键字模糊查询
+            }
+            if(StringUtils.isNoneBlank(district)){
+                  //添加过滤条件，根据地址关键字模糊查询
+            }
+        }
         subareaService.pageQuery(pageBean);
         this.java2Json(pageBean,
                 new String[] { "currentPage", "pageSize", "detachedCriteria", "decidedzone", "subareas" });
