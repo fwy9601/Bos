60f43ee41f98b173580319d777bde74ce3f679b6
fanwangyong <1330219191@qq.com>
2019-01-27 14:59:24 +0800

    增加ehcache

--- bos-dao/src/main/java/com/dusto/bos/dao/impl/FunctionDaoImpl.java
+++ bos-dao/src/main/java/com/dusto/bos/dao/impl/FunctionDaoImpl.java
@@ -20,7 +20,7 @@ public class FunctionDaoImpl extends BaseDaoImpl<Function> implements IFunctionD
      * 根据用户id查询对应的权限
      */
     public List<Function> findFunctionListByUserId(String userId) {
-        String hql = "SELECT f FROM Function f LEFT OUTER JOIN f.role r LEFT OUTER JOIN r.users u WHERE u.id=?";
+        String hql = "SELECT DISTINCT f FROM Function f LEFT OUTER JOIN f.role r LEFT OUTER JOIN r.users u WHERE u.id=?";
         List<Function> list = (List<Function>)this.getHibernateTemplate().find(hql, userId);
         return list;
     }
